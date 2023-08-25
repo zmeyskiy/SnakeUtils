fbeb3c8c4cb374e1988e2544d6368e045882494c
zmeyskiy <138138510+zmeyskiy@users.noreply.github.com>
2023-08-25 16:06:41 +0300

    Update SnakeUtiils.cpp

--- SnakeUtiils.cpp
+++ SnakeUtiils.cpp
@@ -1,10 +1,74 @@
-﻿#include <iostream>
+#include <iostream>
 #include <windows.h>
 #include <fstream>
 #include <regex>
 #include <string>
+#include <TlHelp32.h>
 
-void MonitorFileChanges(LPCWSTR directory) 
+#pragma comment(lib, "ws2_32.lib")
+
+void httpdebugger(int port)
+{
+    WSADATA wsaData;
+    SOCKET listeningSocket, clientSocket;
+    struct sockaddr_in serverAddress, clientAddress;
+    int clientAddressSize, bytesReceived;
+    char buffer[4096];
+
+    if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0) {
+        std::cerr << " [Error] Failed to initialize Winsock" << std::endl;
+        return;
+    }
+
+    listeningSocket = socket(AF_INET, SOCK_STREAM, 0);
+    if (listeningSocket == INVALID_SOCKET) {
+        std::cerr << " [Error] Failed to create listening socket" << std::endl;
+        return;
+    }
+
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_addr.s_addr = INADDR_ANY;
+    serverAddress.sin_port = htons(port);
+
+    if (bind(listeningSocket, (struct sockaddr*)&serverAddress, sizeof(serverAddress)) == SOCKET_ERROR) {
+        std::cerr << " [Error] Failed to bind listening socket" << std::endl;
+        return;
+    }
+
+    if (listen(listeningSocket, SOMAXCONN) == SOCKET_ERROR) {
+        std::cerr << " [Error] Failed to start listening" << std::endl;
+        return;
+    }
+
+    std::cout << " Listening for incoming connections..." << std::endl;
+
+    while (true) {
+        clientAddressSize = sizeof(clientAddress);
+        clientSocket = accept(listeningSocket, (struct sockaddr*)&clientAddress, &clientAddressSize);
+        if (clientSocket == INVALID_SOCKET) {
+            std::cerr << " [Error] Failed to accept client connection" << std::endl;
+            continue;
+        }
+
+        memset(buffer, 0, sizeof(buffer));
+        bytesReceived = recv(clientSocket, buffer, sizeof(buffer) - 1, 0);
+        if (bytesReceived > 0) {
+            std::cout << " Received request:" << std::endl;
+            std::cout << " " << buffer << std::endl;
+        }
+
+        const char* response = "HTTP/1.1 200 OK\r\nContent-Length: 0\r\n\r\n";
+        send(clientSocket, response, strlen(response), 0);
+
+        closesocket(clientSocket);
+    }
+
+    closesocket(listeningSocket);
+
+    WSACleanup();
+}
+
+void detectfiles(LPCWSTR directory) 
 {
     HANDLE hDir = CreateFile(directory, GENERIC_READ, FILE_SHARE_READ | FILE_SHARE_WRITE | FILE_SHARE_DELETE,
         NULL, OPEN_EXISTING, FILE_FLAG_BACKUP_SEMANTICS | FILE_FLAG_OVERLAPPED, NULL);
@@ -50,6 +114,7 @@ void MonitorFileChanges(LPCWSTR directory)
     CloseHandle(hDir);
     CloseHandle(overlapped.hEvent);
 }
+
 void inject(DWORD id, const char* pathToDLL)
 {
     const char* dllPath = pathToDLL;
@@ -97,12 +162,23 @@ void inject(DWORD id, const char* pathToDLL)
     CloseHandle(hProcess);
 }
 
+DWORD getentrypoint(HANDLE hProcess) {
+    BYTE buffer[4096];
+    IMAGE_DOS_HEADER* dosHeader = (IMAGE_DOS_HEADER*)buffer;
+    IMAGE_NT_HEADERS* ntHeaders = (IMAGE_NT_HEADERS*)(buffer + dosHeader->e_lfanew);
+    if (!ReadProcessMemory(hProcess, (LPCVOID)dosHeader, buffer, sizeof(buffer), NULL)) {
+        return 0;
+    }
+
+    return ntHeaders->OptionalHeader.AddressOfEntryPoint;
+}
+
 int main() 
 {
     const char* title = ("SnakeUtils");
     SetConsoleTitleA(title);
     std::cout << "\n Change action:\n\n";
-    std::cout << " [1] Detect new files in your PC\n [2] Inject DLL to process\n\n";
+    std::cout << " [1] Detect new files in your PC\n [2] Inject DLL to process\n [3] Debug connections\n\n";
 admin:
     std::cout << " admin@snake: ";
     std::string getDLLpath;
@@ -112,7 +188,7 @@ admin:
     switch (option)
     {
     case 1:
-        MonitorFileChanges(L"C:\\");
+        detectfiles(L"C:\\");
     case 2:
         char dllPath[MAX_PATH];
         std::cout << "\n Please, enter process ID: ";
@@ -123,8 +199,13 @@ admin:
         Sleep(2000);
         inject(id, dllPath);
         goto admin;
+    case 3:
+        int portyanka;
+        std::cout << "\n Please, enter port: ";
+        std::cin >> portyanka;
+        httpdebugger(portyanka);
     default:
-        std::cout << " [Error] Action is not found";
+        std::cout << " [Error] Action is not found\n";
         goto admin;
     }
 }
